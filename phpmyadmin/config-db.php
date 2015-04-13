213a770eb35df1e3fdb0e374519fdbaee495b733
海洋大學網路發展協會網站托管服務系統管理員 <ntouind@gmail.com>
2015-04-13 10:41:25 +0000

    committing changes in /etc after apt run
    Package changes:
    +dbconfig-common 1.8.47+nmu1
    +javascript-common 11
    +libjs-codemirror 2.23-1
    +libjs-jquery 1.7.2+dfsg-2ubuntu1
    +libjs-jquery-cookie 8-2
    +libjs-jquery-event-drag 8-2
    +libjs-jquery-metadata 8-2
    +libjs-jquery-mousewheel 8-2
    +libjs-jquery-tablesorter 8-2
    +libjs-jquery-ui 1.10.1+dfsg-1
    +libjs-underscore 1.4.4-2ubuntu1
    +libmcrypt4 2.5.8-3.1ubuntu1
    +php-gettext 1.0.11-1
    +php5-gd 5.5.9+dfsg-1ubuntu4
    +php5-mcrypt 5.4.6-0ubuntu5
    +phpmyadmin 4:4.0.10-1

---
+++ phpmyadmin/config-db.php
@@ -0,0 +1,19 @@
+<?php
+##
+## database access settings in php format
+## automatically generated from /etc/dbconfig-common/phpmyadmin.conf
+## by /usr/sbin/dbconfig-generate-include
+## Mon, 13 Apr 2015 10:41:23 +0000
+##
+## by default this file is managed via ucf, so you shouldn't have to
+## worry about manual changes being silently discarded.  *however*,
+## you'll probably also want to edit the configuration file mentioned
+## above too.
+##
+$dbuser='phpmyadmin';
+$dbpass='R3Nv7WWF1i49';
+$basepath='';
+$dbname='phpmyadmin';
+$dbserver='';
+$dbport='';
+$dbtype='mysql';
